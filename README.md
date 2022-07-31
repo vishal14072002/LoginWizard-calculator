03b5e1a1eb93e74d7265377e68b20c03188df7eb
Vishal Kumar Srivastava <79965974+vishal14072002@users.noreply.github.com>
2022-07-31 21:55:57 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Register and Login Page
+ Register and Login Page using Node.js and mongodb
---
+++ app.js
@@ -0,0 +1,104 @@
+const express               =  require('express'),
+      app                   =  express(),
+      mongoose              =  require("mongoose"),
+      passport              =  require("passport"),
+      bodyParser            =  require("body-parser"),
+      LocalStrategy         =  require("passport-local"),
+      passportLocalMongoose =  require("passport-local-mongoose"),
+      User                  =  require("./models/user");
+
+
+//Connecting database
+// Module calling
+mongoose.connect('mongodb://localhost/vishal');
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function callback () {
+  console.log("h");
+});
+
+exports.test = function(req,res) {
+  res.render('vishal');
+};
+
+
+
+app.use(require("express-session")({
+    secret:"Any normal Word",       //decode or encode session
+    resave: false,          
+    saveUninitialized:false    
+}));
+
+passport.serializeUser(User.serializeUser());       //session encoding
+passport.deserializeUser(User.deserializeUser());   //session decoding
+passport.use(new LocalStrategy(User.authenticate()));
+app.set("view engine","ejs");
+app.use(bodyParser.urlencoded(
+      { extended:true }
+))
+app.use(passport.initialize());
+app.use(passport.session());
+
+
+
+
+app.get("/", (req,res) =>{
+    res.render("home");
+})
+
+app.get("/userprofile",isLoggedIn ,(req,res) =>{
+    res.render("userprofile");
+})
+//Auth Routes
+app.get("/login",(req,res)=>{
+    res.render("login");
+});
+
+app.post("/login",passport.authenticate("local",{
+    successRedirect:"/userprofile",
+    failureRedirect:"/login"
+}),function (req, res){
+
+});
+
+app.get("/register",(req,res)=>{
+    res.render("register");
+});
+
+app.post("/register",(req,res)=>{
+    
+    User.register(new User({username: req.body.username,phone:req.body.phone,telephone: req.body.telephone}),req.body.password,function(err,user){
+        if(err){
+            console.log(err);
+            res.render("register");
+        }
+    passport.authenticate("local")(req,res,function(){
+        res.redirect("/login");
+    })    
+    })
+})
+
+app.get("/logout",(req,res)=>{
+    req.logout();
+    res.redirect("/");
+});
+
+function isLoggedIn(req,res,next) {
+    if(req.isAuthenticated()){
+        return next();
+    }
+    res.redirect("/login");
+}
+
+
+//Listen On Server
+
+
+app.listen(process.env.PORT ||8080,function (err) {
+    if(err){
+        console.log(err);
+    }else {
+        console.log("Server Started At Port 8080");
+    }
+      
+});
